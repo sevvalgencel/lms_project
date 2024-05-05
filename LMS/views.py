3857965afe15c58debd63d1620ae85a4127a931c
Şevval Gencel <gencels14@gmail.com>
2024-05-05 13:41:08 +0300

    Category Model Created

--- LMS/views.py
+++ LMS/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import redirect,render
-
+from app.models import Categories
 def BASE(request):
     return render(request,'base.html')
 
 def HOME(request):
-    return render(request,'Main/home.html')
+    category = Categories.objects.all().order_by('id')[0:5]
+
+    context = {
+        'category' : category,
+    }
+    return render(request,'Main/home.html',context)
 
 def SINGLE_COURSE(request):
     return render(request,'Main/single_course.html')
